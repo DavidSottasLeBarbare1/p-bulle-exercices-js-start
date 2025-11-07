3e2ea49729093d389e931050f18c15e6db00caf2
David Sottas <pk88sdk@eduvaud.ch>
2025-11-07 16:30:49 +0100

    feat(bob) exercice commencé
    [40] [DONE]

--- exercices/bob/bob.js
+++ exercices/bob/bob.js
@@ -4,5 +4,23 @@
 //
 
 export const hey = (message) => {
-  throw new Error('Remove this line and implement the function');
+  if (message.trim().endsWith("?"))
+  {
+    return "Sure.";
+  }
+  if (/[a-zA-Z]/.test(message))
+  {
+    if (message === message.toUpperCase() && message.trim().endsWith("?"))
+    {
+        return "Calm down, I know what I'm doing!";
+    }
+    if (message === message.toUpperCase())
+    {
+      return "Whoa, chill out!";
+    }
+    else 
+      return "Whatever.";
+  }
+return "Fine. Be that way!";
+  
 };
