6a8fe38791a9fb14d5769a49c39d8fdbc133abdd
David Sottas <pk88sdk@eduvaud.ch>
2025-11-14 16:15:56 +0100

    feat(space-age): terminer l'exercice
    [30] [DONE]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+//Map containing planets with their specific year
+const planets = new Map([
+    ["mercury", 0.2408467],
+    ["venus", 0.61519726],
+    ["earth", 1],
+    ["mars", 1.8808158],
+    ["jupiter", 11.862615],
+    ["saturn", 29.447498],
+    ["uranus", 84.016846],
+    ["neptune", 164.79132]
+])
+export const age = (planet, secondsOld) => {
+  //Check if planet is inside planets
+  if (!planets.get(planet))
+    throw new Error("not a planet");
+
+  //Return the age
+  return Math.round((secondsOld / 31557600)/planets.get(planet) * 100) / 100
 };
