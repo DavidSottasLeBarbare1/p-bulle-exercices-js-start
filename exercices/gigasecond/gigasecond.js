10310fc1bf454184bfdef56e262cfb0a75807458
David Sottas <pk88sdk@eduvaud.ch>
2025-11-21 13:59:18 +0100

    feat(gigasecond): terminer l'exercice
    [40] [DONE]

--- exercices/gigasecond/gigasecond.js
+++ exercices/gigasecond/gigasecond.js
@@ -3,6 +3,10 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error('Remove this line and implement the function');
+export const gigasecond = (date) => {
+  //Converting date in epoch then adding a gigasecond
+  dateInEpoch = Math.floor(date.getTime()/1000.0) + 1000000000
+
+  //Returning a new date
+  return new Date(dateInEpoch * 1000)
 };
