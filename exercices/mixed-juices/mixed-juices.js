47ee5fa7865048e8a7770f7c73c31f29dfee9ab3
David Sottas <pk88sdk@eduvaud.ch>
2025-11-21 14:43:26 +0100

    feat(mixed-juices): terminer l'exercice
    [35] [DONE]

--- exercices/mixed-juices/mixed-juices.js
+++ exercices/mixed-juices/mixed-juices.js
@@ -11,7 +11,19 @@
  * @returns {number} time in minutes
  */
 export function timeToMixJuice(name) {
-  throw new Error('Remove this line and implement the function');
+  //Check the name of the juice then returning the time
+  switch (name){
+    case "Pure Strawberry Joy":
+      return 0.5;
+    case "Energizer": case "Green Garden":
+      return 1.5;
+    case "Tropical Island":
+      return 3;
+    case "All or Nothing":
+      return 5;
+    default:
+      return 2.5;
+  }
 }
 
 /**
@@ -23,7 +35,35 @@ export function timeToMixJuice(name) {
  * @returns {number} number of limes cut
  */
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Remove this line and implement the function');
+  //Declaring the wedgesMissing and the numberOfLimes variable
+  let wedgesMissing = wedgesNeeded
+  let numberOfLimes = 0;
+
+  //As long as wedgesMissing is higher than 0, removes the correct wedges from wedgesMissing according to the size of the lime in limes
+  while (wedgesMissing > 0)
+  {
+    switch (limes[0])
+    {
+      case 'small':
+        wedgesMissing -= 6;
+        numberOfLimes++;
+        limes.shift()
+        break;
+      case 'medium':
+        wedgesMissing -= 8;
+        numberOfLimes++;
+        limes.shift()
+        break;
+      case 'large':
+        wedgesMissing -= 10;
+        numberOfLimes++;
+        limes.shift()
+        break;
+      default:
+        return numberOfLimes;
+    }
+  }
+  return numberOfLimes;
 }
 
 /**
@@ -34,5 +74,11 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Remove this line and implement the function');
+  //Remove the time from timeLeft according to the time of each juice then removing that juice until there's no more time
+  do{
+    timeLeft -= timeToMixJuice(orders[0])
+    orders.shift()
+  }
+  while (timeLeft > 0)
+  return orders
 }
