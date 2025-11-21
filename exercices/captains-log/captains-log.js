e0f07eec94e70d238641d9e0e9f66027d5887451
David Sottas <pk88sdk@eduvaud.ch>
2025-11-21 15:57:07 +0100

    feat(captains-log): terminer l'exercice
    [10] [DONE]

--- exercices/captains-log/captains-log.js
+++ exercices/captains-log/captains-log.js
@@ -6,9 +6,8 @@
  * @returns {string} the generated registry number.
  */
 export function randomShipRegistryNumber() {
-  throw new Error(
-    'Please remove this line and implement the randomShipRegistryNumber() function',
-  );
+  //Returning NCC- + a random number between 9999 and 1000 both inclusive
+  return "NCC-" + Math.floor(1000 + Math.random() * (10000 - 1000));
 }
 
 /**
@@ -17,9 +16,8 @@ export function randomShipRegistryNumber() {
  * @returns {number} a stardate between 41000 (inclusive) and 42000 (exclusive).
  */
 export function randomStardate() {
-  throw new Error(
-    'Please remove this line and implement the randomStardate() function',
-  );
+  //Generate a random number between 42000 and 41000
+  return 41000 + Math.random() * (42000 - 41000);
 }
 
 /**
@@ -28,7 +26,9 @@ export function randomStardate() {
  * @returns {string} a one-letter planet class.
  */
 export function randomPlanetClass() {
-  throw new Error(
-    'Please remove this line and implement the randomStardate() function',
-  );
+  //List of all planet classes
+  let classList = "DHJKLMNRTY";
+
+  //Returning a random class from classList
+  return classList[Math.floor(Math.random() * classList.length)];
 }
