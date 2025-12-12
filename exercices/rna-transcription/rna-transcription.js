a049a0caa900d211a17a8657a4c027dc63d8fd1c
David Sottas <pk88sdk@eduvaud.ch>
2025-12-12 16:26:31 +0100

    feat(rna-transcription): terminer l'exercice
    [10] [DONE]

--- exercices/rna-transcription/rna-transcription.js
+++ exercices/rna-transcription/rna-transcription.js
@@ -2,7 +2,17 @@
 // This is only a SKELETON file for the 'RNA Transcription' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-
-export const toRna = () => {
-  throw new Error('Remove this line and implement the function');
+const dnaMap = new Map([
+  ["G", "C"],
+  ["C", "G"],
+  ["T", "A"],
+  ["A", "U"],
+]);
+export const toRna = (DNA) => {
+  //Assigning the whole RNA sequence to the fullRNA variable using charAt and the dnaMap Map
+  let fullRNA = "";
+  for (let i = 0; i < DNA.length; i++) {
+    fullRNA += dnaMap.get(DNA.charAt(i));
+  }
+  return fullRNA;
 };
