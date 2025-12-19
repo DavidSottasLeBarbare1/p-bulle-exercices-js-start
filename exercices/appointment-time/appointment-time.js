d7c4328bfd3b62a5f6ad81bba1081b0d13144195
David Sottas <pk88sdk@eduvaud.ch>
2025-12-19 14:04:12 +0100

    feat(appointment-time): terminer l'exercice
    [50] [DONE]

--- exercices/appointment-time/appointment-time.js
+++ exercices/appointment-time/appointment-time.js
@@ -9,7 +9,21 @@
  * @returns {Date} the appointment
  */
 export function createAppointment(days, now = undefined) {
-  throw new Error('Remove this line and implement the function');
+  //Assigning the current time to currentTime
+  let currentTime = Date.now();
+
+  //If now isnt undefined, assign currentTime to it
+  if (now != undefined) {
+    currentTime = now;
+  }
+
+  //Assigning a variable to the timezone offset
+  let timeZoneOffset =
+    new Date(currentTime + days * 86400000).getTimezoneOffset() -
+    new Date(currentTime).getTimezoneOffset();
+
+  //Return the appointment date based on the timezone, currentTime, the days to offset the appointment to (in milliseconds)
+  return new Date(timeZoneOffset * 60000 + currentTime + days * 86400000);
 }
 
 /**
@@ -20,7 +34,8 @@ export function createAppointment(days, now = undefined) {
  * @returns {string} timestamp
  */
 export function getAppointmentTimestamp(appointmentDate) {
-  throw new Error('Remove this line and implement the function');
+  //Uses the toISOString function to generate the timestamp of appointmentDate
+  return new Date(appointmentDate).toISOString();
 }
 
 /**
@@ -31,7 +46,17 @@ export function getAppointmentTimestamp(appointmentDate) {
  * @returns {Record<'year' | 'month' | 'date' | 'hour' | 'minute', number>} the appointment details
  */
 export function getAppointmentDetails(timestamp) {
-  throw new Error('Remove this line and implement the function');
+  //Assign the date to a constant
+  const date = new Date(timestamp);
+
+  //Return every extracted information about the date
+  return {
+    year: date.getFullYear(),
+    month: date.getMonth(),
+    date: date.getDate(),
+    hour: date.getHours(),
+    minute: date.getMinutes(),
+  };
 }
 
 /**
@@ -43,7 +68,17 @@ export function getAppointmentDetails(timestamp) {
  * @returns {Record<'year' | 'month' | 'date' | 'hour' | 'minute', number>} the appointment details
  */
 export function updateAppointment(timestamp, options) {
-  throw new Error('Remove this line and implement the function');
+  //Assigning year, month etc... to the extracted informations and changing them based on options
+  const { year, month, date, hour, minute } = {
+    ...getAppointmentDetails(timestamp),
+    ...options,
+  };
+
+  //Declaring the new date that we would like to return
+  let changedDate = new Date(year, month, date, hour, minute);
+
+  //Using the getAppointmentDetails function to return the details of changedDate so it doesn't look as ugly as before
+  return getAppointmentDetails(changedDate.toISOString());
 }
 
 /**
@@ -55,7 +90,10 @@ export function updateAppointment(timestamp, options) {
  * @returns {number} amount of seconds (rounded)
  */
 export function timeBetween(timestampA, timestampB) {
-  throw new Error('Remove this line and implement the function');
+  //Return the rounded difference between timestampA and timestampB
+  return Math.ceil(
+    (new Date(timestampB).getTime() - new Date(timestampA).getTime()) / 1000
+  );
 }
 
 /**
@@ -65,5 +103,6 @@ export function timeBetween(timestampA, timestampB) {
  * @param {string} currentTimestamp (ISO 8601)
  */
 export function isValid(appointmentTimestamp, currentTimestamp) {
-  throw new Error('Remove this line and implement the function');
+  //Returns the correct boolean according to appointmentTimestamp and currentTimestamp
+  return new Date(appointmentTimestamp) > new Date(currentTimestamp);
 }
