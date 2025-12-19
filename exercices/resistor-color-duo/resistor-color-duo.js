9a375627418cb9e7be1039a3f59a114fd3d7da2e
David Sottas <pk88sdk@eduvaud.ch>
2025-12-19 16:26:32 +0100

    feat(resistor-color-duo): terminer l'exercice
    [15] [DONE]

--- exercices/resistor-color-duo/resistor-color-duo.js
+++ exercices/resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,21 @@
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error('Remove this line and implement the function');
+export const decodedValue = (colors) => {
+  //Returning the correct resistance
+  return COLORS.indexOf(colors[0]) * 10 + COLORS.indexOf(colors[1]);
 };
+
+//Assigning the COLORS constant
+export const COLORS = [
+  "black",
+  "brown",
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "violet",
+  "grey",
+  "white",
+];
